ddf7950a5c526839387484b4e2d270e34f9c12f4
Gibbyswag <will.gibson7@gmail.com>
2020-03-31 18:27:11 -0700

    updated folder structure and fixed local storage based on TA feedback

--- script.js
+++ script.js
@@ -6,17 +6,19 @@ console.log(today)
 var now = moment().format("H");
 console.log(now)
 
-var plannerDayHours = [
-    {number: 9, hour: "9 AM", event: "-"},
-    {number: 10, hour: "10 AM", event: "-"},
-    {number: 11, hour: "11 AM", event: "-"},
-    {number: 12, hour: "12 PM", event: "-"},
-    {number: 13, hour: "1 PM", event: "-"},
-    {number: 14, hour: "2 PM", event: "-"},
-    {number: 15, hour: "3 PM", event: "-"},
-    {number: 16, hour: "4 PM", event: "-"},
-    {number: 17, hour: "5 PM", event: "-"},
-];
+var plannerDayHours = {"BusinessHours": [
+{number: 9, hour: "9 AM", event: "-"},
+{number: 10, hour: "10 AM", event: "-"},
+{number: 11, hour: "11 AM", event: "-"},
+{number: 12, hour: "12 PM", event: "-"},
+{number: 13, hour: "1 PM", event: "-"},
+{number: 14, hour: "2 PM", event: "-"},
+{number: 15, hour: "3 PM", event: "-"},
+{number: 16, hour: "4 PM", event: "-"},
+{number: 17, hour: "5 PM", event: "-"}
+
+]}; 
+    
 
 // Check local storage for previously saved planner acitivites, if yes then fill current
 var checkPrevious = JSON.parse(localStorage.getItem("plannerDayHours"));
@@ -27,13 +29,13 @@ if (checkPrevious !== null) {
 
 $("#currentDay").text(today)
 
-for (var i = 0; i < plannerDayHours.length; i++) {
+for (var i = 0; i < plannerDayHours.BusinessHours.length; i++) {
     var newTR = $("<tr>newTR</tr>");
     newTR.addClass("row");
     $(".container").append(newTR);
 
-    var hourDisplay = $("<td>" + plannerDayHours[i].hour + "</td>");
-    var eventDisplay = $("<td><textarea>" + plannerDayHours[i].event + "</textarea></td>");
+    var hourDisplay = $("<td>" + plannerDayHours.BusinessHours[i].hour + "</td>");
+    var eventDisplay = $('<td><textarea id="test">' + plannerDayHours.BusinessHours[i].event + '</textarea></td>');
     var saveDisplay = $("<td>Save</td>");
 
     hourDisplay.addClass("hour");
@@ -41,15 +43,15 @@ for (var i = 0; i < plannerDayHours.length; i++) {
     saveDisplay.addClass("saveBtn");
     saveDisplay.attr("id", "li" + i);
 
-    if (plannerDayHours[i].number == now) {
+    if (plannerDayHours.BusinessHours[i].number == now) {
         eventDisplay.addClass("present");
     }
 
-    if (plannerDayHours[i].number > now) {
+    if (plannerDayHours.BusinessHours[i].number > now) {
         eventDisplay.addClass("future");
     }
 
-    else if (plannerDayHours[i].number < now) {
+    else if (plannerDayHours.BusinessHours[i].number < now) {
         eventDisplay.addClass("past");
     }
 
@@ -57,16 +59,22 @@ for (var i = 0; i < plannerDayHours.length; i++) {
 
 };
 
-saveDisplay.on("click", function () {
+$(document).on("click", ".saveBtn", function () {
     var dataID = $(this).attr("id");
+    console.log(dataID)
 
     var index = dataID.split("li")[1];
+    console.log(index)
 
-    newEvent = $("#" + dataID).val();
+    newEvent = $("#test").val();
+    console.log(newEvent)
 
-    plannerDayHours[index].event = newEvent;
+    plannerDayHours.BusinessHours[index].event = newEvent;
+    console.log(plannerDayHours.BusinessHours[index].event)
+    console.log(newEvent)
 
     localStorage.setItem("plannerDayHours", JSON.stringify(plannerDayHours));
+    console.log(localStorage)
 
 });  
 
